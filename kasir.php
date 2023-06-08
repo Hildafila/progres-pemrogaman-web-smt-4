fdef1161b0979b2e809ec455912803ee9d4444a5
Hildafila <120363121+Hildafila@users.noreply.github.com>
2023-06-08 14:37:47 +0700

    Add files via upload

---
+++ kasir.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class kasir extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kasir';
+    protected $primarykey ='idkasir';
+
+}
